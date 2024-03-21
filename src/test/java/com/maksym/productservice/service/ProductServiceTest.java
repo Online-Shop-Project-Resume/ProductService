05d4d173c47a28c9253c246dc1bf00610ff709a5
Maksym Makhrevych <makhrevychmaxim@gmail.com>
2024-03-20 20:47:23 -0500

    Removed unneccessary dependencies

--- src/test/java/com/maksym/productservice/service/ProductServiceTest.java
+++ src/test/java/com/maksym/productservice/service/ProductServiceTest.java
@@ -7,9 +7,11 @@ import com.maksym.productservice.repository.ProductRepository;
 import com.maksym.productservice.staticObject.StaticProduct;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     @Mock
